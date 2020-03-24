b84125a6dd90e3c119f76aba7d76e7c0d0e28dec
slpMouse <slpMouse@yandex.ru>
2020-03-24 10:37:31 +0300

    Some changes and refactoring

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,4 +1,4 @@
-'use strict';
+`use strict`;
 
 /**
  * TODO: Разбить на класс Note
@@ -8,24 +8,37 @@
 $(`document`).ready(function () {
 
   class App {
+    /**
+     * Класс App отвечает за управлением всем приложением
+     */
+
     constructor() {
+      /**
+       * Инициализируем необходимые элементы
+       */
+
       this.noteList = $(`#js-notesList`);
       this.middleCollapsing = $(`#js-collapseMiddle`);
+
       this.editorTitle = $(`#js-editorTitle`);
-      // this.editorFrame = document.getElementById(`js-editorFrame`);
-      // this.editorFrameDoc = this.editorFrame.contentDocument;
-      // this.editorFrameWin = this.editorFrame.contentWindow;
       this.editorFrame = $(`#js-editorFrame`)[0];
       this.editorFrameDoc = this.editorFrame.contentDocument;
       this.editorFrameWin = this.editorFrame.contentWindow;
+
       this.formattingTools = $(`#js-formattingTools`);
       this.activeNote = false;
 
+      /** Инициализируем фрейм */
       this.initilizeFrame();
+      /** Инициализируем обработчики событий */
       this.initilizeHandlers();
     }
 
     initilizeFrame() {
+      /**
+       * Инициализация Фрейм - Создание его разметки, запись и активация режима проектирования
+       */
+
       /** TODO: Здесь мы подгружаем нашу заметку */
       // this.note.loadNote();
 
@@ -35,36 +48,43 @@ $(`document`).ready(function () {
       this.editorFrameDoc.close();
       this.editorFrameDoc.designMode = `on`;
 
-      // Сохраняем body
       this.editorFrameBody = this.editorFrameDoc.body;
     }
 
     initilizeHandlers() {
       /**
-       * Обработчик событий на клик по заметке
+       * Инициализируем базовые обработчики событий
+       */
+
+      /**
+       * Клик по заметке
        */
       $(`.list__item`).click(function () { _app.openNote(this) });
 
       /**
-       * Обработчик событий на элементы редактирования текста
+       * Клик на кнопки форматирования
        */
       $(`.editor__formatting-tool`).mousedown(function () { return _app.formatText(this); });
 
-      // $(document.getElementsByTagName('iframe')[0].contentDocument.body).keyup(function () { console.log($(this)[0].innerHTML) })
-      // $(this.editorFrameBody).keyup(function () { console.log($(this)[0].innerHTML) })
-
-      /** Обработчик изменения frame`а */
-      // $(this.editorFrameBody).keyup(this.frameChangeHandler);
+      /**
+       * Нажатие кнопки внутри Фрейма
+       */
       $(this.editorFrameBody).keyup(function () { _app.frameChangeHandler() })
     }
 
     openNote(clickedNote) {
       /**
-       * Если выбранная заметка уже была открыта (имеет класс active)
-       * закроем поле редактирования и удалим класс active
+       * Если выбранная заметка уже была открыта (имеет класс active), то
+       * Закроем поле редактирования и удалим класс active
        *
-       * Иначе откроем поле редактирования и добавим класс active
+       * Иначе откроем поле редактирования и добавим класс выбранной заметке active
        */
+
+      /** TODO: Здесь подгружается Фрейм с содержимым заметки, реализовать методы:
+       * this.noteId = this.activeNote.attr(`data-id`);
+       * this.reloadFrame();
+       */
+
       if ($(clickedNote).hasClass(`active`)) {
         $(this.middleCollapsing).addClass(`hidden`);
         this.activeNote.removeClass(`active`),
@@ -75,17 +95,22 @@ $(`document`).ready(function () {
           this.activeNote = $(clickedNote);
         this.activeNote.addClass(`active`);
       }
-
-      /** TODO: Здесь подгружается Фрейм с содержимым заметки */
-      // this.noteId = this.activeNote.attr(`data-id`);
-      // this.reloadFrame();
     }
 
     frameChangeHandler() {
-      this.editorFrameBody.innerHTML ? this.formattingTools.removeClass('hidden') : this.formattingTools.addClass('hidden');
+      /**
+       * Обработчик нажатия кнопок внутри фрейма
+       */
+
+      /** Если после изменения фрейма он стал пустым, убрать блок с кнопками форматирования, иначе показать его */
+      this.editorFrameBody.innerHTML ? this.formattingTools.removeClass(`hidden`) : this.formattingTools.addClass(`hidden`);
     }
 
     formatText(tool) {
+      /**
+       * Обработчик кнопок форматирования
+       */
+
       this.editorFrameWin.focus();
       const action = $(tool).attr(`data-action`);                     // Действие для обработки текста
       const param = $(tool).attr(`data-param`) || null;               // Параметры если есть
@@ -95,7 +120,6 @@ $(`document`).ready(function () {
     }
   }
 
+  /** Инициализируем объект приложения */
   _app = new App;
-
-  // TODO: Сделать создание фрейма и его обработку
 });
