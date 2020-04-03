b2b1610d4de28a263404519c41394ab4c61772c1
slpMouse <slpMouse@yandex.ru>
2020-04-03 19:09:21 +0300

    More design added
    Maybe someday I`ll start writing normal commits, but not today... not today

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const gulp = require(`gulp`);
-const watch = require(`gulp-watch`);
-const prefixer = require(`gulp-autoprefixer`);
-const uglify = require(`gulp-uglify-es`).default;
-const sass = require(`gulp-sass`);
-const sourcemaps = require(`gulp-sourcemaps`);
-const rigger = require(`gulp-rigger`);
-const cssmin = require(`gulp-minify-css`);
-const rename = require(`gulp-rename`);
-const imagemin = require(`gulp-imagemin`);
-const pngquant = require(`imagemin-pngquant`);
-const rimraf = require(`rimraf`);
-const browserSync = require(`browser-sync`);
+const gulp = require( `gulp` );
+const watch = require( `gulp-watch` );
+const prefixer = require( `gulp-autoprefixer` );
+const uglify = require( `gulp-uglify-es` ).default;
+const sass = require( `gulp-sass` );
+const sourcemaps = require( `gulp-sourcemaps` );
+const rigger = require( `gulp-rigger` );
+const cssmin = require( `gulp-minify-css` );
+const rename = require( `gulp-rename` );
+const imagemin = require( `gulp-imagemin` );
+const pngquant = require( `imagemin-pngquant` );
+const rimraf = require( `rimraf` );
+const browserSync = require( `browser-sync` );
 const reload = browserSync.reload;
 
 var path = {
@@ -56,78 +56,78 @@ var config = {
     logPrefix: `slpServer`
 };
 
-gulp.task(`webserver`, function () {
-    browserSync(config);
-});
+gulp.task( `webserver`, function () {
+    browserSync( config );
+} );
 
-gulp.task(`clean`, function (cb) {
-    return rimraf(path.clean, cb);
-});
+gulp.task( `clean`, function ( cb ) {
+    return rimraf( path.clean, cb );
+} );
 
-gulp.task(`html:build`, function () {
-    return gulp.src(path.src.html)
-        .pipe(rigger())
-        .pipe(gulp.dest(path.build.html))
-        .pipe(reload({ stream: true }));
-});
+gulp.task( `html:build`, function () {
+    return gulp.src( path.src.html )
+        .pipe( rigger() )
+        .pipe( gulp.dest( path.build.html ) )
+        .pipe( reload( { stream: true } ) );
+} );
 
-gulp.task(`js:build`, function () {
-    return gulp.src(path.src.js)
-        .pipe(rigger())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.js))
-        .pipe(reload({ stream: true }));
-});
+gulp.task( `js:build`, function () {
+    return gulp.src( path.src.js )
+        .pipe( rigger() )
+        .pipe( sourcemaps.init() )
+        .pipe( uglify() )
+        .pipe( sourcemaps.write() )
+        .pipe( gulp.dest( path.build.js ) )
+        .pipe( reload( { stream: true } ) );
+} );
 
-gulp.task(`style:build`, function () {
-    return gulp.src(path.src.style)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
+gulp.task( `style:build`, function () {
+    return gulp.src( path.src.style )
+        .pipe( sourcemaps.init() )
+        .pipe( sass( {
             sourceMap: true,
             errLogToConsole: true
-        }))
-        .pipe(prefixer())
-        .pipe(cssmin())
-        .pipe(sourcemaps.write())
-        .pipe(rename({ suffix: `.min` }))
-        .pipe(gulp.dest(path.build.css))
-        .pipe(reload({ stream: true }));
-});
+        } ) )
+        .pipe( prefixer() )
+        .pipe( cssmin() )
+        .pipe( sourcemaps.write() )
+        .pipe( rename( { suffix: `.min` } ) )
+        .pipe( gulp.dest( path.build.css ) )
+        .pipe( reload( { stream: true } ) );
+} );
 
-gulp.task(`image:build`, function () {
-    return gulp.src(path.src.img)
-        .pipe(imagemin({
+gulp.task( `image:build`, function () {
+    return gulp.src( path.src.img )
+        .pipe( imagemin( {
             progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            use: [pngquant()],
+            svgoPlugins: [ { removeViewBox: false } ],
+            use: [ pngquant() ],
             interlaced: true
-        }))
-        .pipe(gulp.dest(path.build.img))
-        .pipe(reload({ stream: true }));
-});
+        } ) )
+        .pipe( gulp.dest( path.build.img ) )
+        .pipe( reload( { stream: true } ) );
+} );
 
-gulp.task(`fonts:build`, function () {
-    return gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts));
-});
+gulp.task( `fonts:build`, function () {
+    return gulp.src( path.src.fonts )
+        .pipe( gulp.dest( path.build.fonts ) );
+} );
 
-gulp.task(`build`, gulp.series(`html:build`, `js:build`, `style:build`, `fonts:build`, `image:build`));
-
-
-gulp.task(`watch`, function () {
-    gulp.watch([path.watch.html], gulp.series(`html:build`));
-    gulp.watch([path.watch.style], gulp.series(`style:build`));
-    gulp.watch([path.watch.js], gulp.series(`js:build`));
-    gulp.watch([path.watch.img], gulp.series(`image:build`));
-    gulp.watch([path.watch.fonts], gulp.series(`fonts:build`));
-});
-
-gulp.task(`css:copy`, function () {
-    return gulp.src(`src/style/*.css`)
-        .pipe(gulp.dest(path.build.css));
-});
+gulp.task( `build`, gulp.series( `html:build`, `js:build`, `style:build`, `fonts:build`, `image:build` ) );
 
 
-gulp.task(`default`, gulp.series(`clean`, gulp.parallel(`build`), `css:copy`, gulp.parallel(`watch`, `webserver`)));
+gulp.task( `watch`, function () {
+    gulp.watch( [ path.watch.html ], gulp.series( `html:build` ) );
+    gulp.watch( [ path.watch.style ], gulp.series( `style:build` ) );
+    gulp.watch( [ path.watch.js ], gulp.series( `js:build` ) );
+    gulp.watch( [ path.watch.img ], gulp.series( `image:build` ) );
+    gulp.watch( [ path.watch.fonts ], gulp.series( `fonts:build` ) );
+} );
+
+gulp.task( `css:copy`, function () {
+    return gulp.src( `src/style/*.css` )
+        .pipe( gulp.dest( path.build.css ) );
+} );
+
+
+gulp.task( `default`, gulp.series( `clean`, gulp.parallel( `build` ), `css:copy`, gulp.parallel( `watch`, `webserver` ) ) );
