4d729399465c1666888a55b21b2ca0cdbc679808
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-07-22 23:01:01 +0300

    Initial commit for version 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,124 +1,98 @@
-'use strict';
+'use strict'
 
-const gulp = require( `gulp` );
-const autoprefixer = require( `autoprefixer` );
-const browserSync = require( `browser-sync` );
-const reload = browserSync.reload;
-const sass = require( `gulp-sass` );
-const csso = require( `gulp-csso` );
-const includer = require( `gulp-file-includer` );
-const rename = require( `gulp-rename` );
-const sourcemaps = require( `gulp-sourcemaps` );
-const uglify = require( `gulp-uglify-es` ).default;
-const watch = require( `gulp-watch` );
-const imagemin = require( `gulp-imagemin` );
-const pngquant = require( `imagemin-pngquant` );
-const rimraf = require( `rimraf` );
-const postcss = require( `gulp-postcss` )
-const rigger = require( `gulp-rigger` )
+const fs = require( 'fs' )
 
-const path = {
-  root: `./dist`,
-  dist: {
-    html: `dist/`,
-    js: `dist/js/`,
-    css: `dist/css/`,
-    img: `dist/img/`,
-    fonts: `dist/webfonts/`
-  },
-  src: {
-    html: `src/*.html`,
-    js: `src/js/*.js`,
-    style: `src/style/*.scss`,
-    img: `src/img/**/*.*`,
-    fonts: `src/webfonts/**/*.*`,
-    cssLib: `src/style/libs/*.css`
-  },
-  watch: {
-    html: [ `src/*.html`, `src/templates/*.html` ],
-    js: [ `src/js/**/*.js` ],
-    style: [ `src/style/**/*.scss` ],
-    img: [ `src/img/**/*.*` ],
-    fonts: [ `src/webfonts/**/*.*` ],
-    cssLib: [ `src/style/libs/*.css` ]
-  }
-}
+const gulp = require( 'gulp' )
+
+const sassCompiler = require( 'gulp-sass' )( require( 'node-sass' ) )
+const typescriptCompiler = require( 'gulp-typescript' )
+const pug2html = require( 'gulp-pug' )
+const BS = require( 'browser-sync' )
 
 const config = {
-  server: { baseDir: path.root },
-  tunnel: false,
-  host: `localhost`,
-  port: 9000,
-  logPrefix: `slpServer`
+  src: './src',
+  dist: './dist',
+  distAssets: './dist/assets',
 }
 
-gulp.task( `server:lunch`, function () {
-  return browserSync( config );
-} );
 
-gulp.task( `clean`, function ( cb ) {
-  return rimraf( path.root, cb );
-} );
 
-gulp.task( `html:build`, function () {
-  return gulp.src( path.src.html )
-    .pipe( includer( { basePath: 'src/' } ) )
-    .pipe( gulp.dest( path.dist.html ) )
-    .pipe( reload( { stream: true } ) );
-} );
-
-gulp.task( `js:build`, function () {
-  return gulp.src( path.src.js )
-    .pipe( rigger() )
-    .pipe( sourcemaps.init() )
-    .pipe( uglify() )
-    .pipe( sourcemaps.write() )
-    .pipe( gulp.dest( path.dist.js ) )
-    .pipe( reload( { stream: true } ) );
-} );
-
-gulp.task( `style:build`, function () {
-  return gulp.src( path.src.style )
-    .pipe( sourcemaps.init() )
-    .pipe( sass() )
-    .pipe( postcss( [ autoprefixer() ] ) )
-    .pipe( csso() )
-    .pipe( sourcemaps.write() )
-    .pipe( rename( { suffix: `.min` } ) )
-    .pipe( gulp.dest( path.dist.css ) )
-    .pipe( reload( { stream: true } ) );
-} );
-
-gulp.task( `image:build`, function () {
-  return gulp.src( path.src.img )
-    .pipe( imagemin( {
-      progressive: true,
-      use: [ pngquant() ],
-      interlaced: true
+function compilePug() {
+  return gulp.src( config.src + '/pages/*.pug' )
+    .pipe( pug2html( {
+      pretty: true,
     } ) )
-    .pipe( gulp.dest( path.dist.img ) )
-    .pipe( reload( { stream: true } ) );
-} );
+    .pipe( gulp.dest( config.dist ) )
+}
 
-gulp.task( `fonts:build`, function () {
-  return gulp.src( path.src.fonts )
-    .pipe( gulp.dest( path.dist.fonts ) );
-} );
+function compileTypescript() {
+  return gulp.src( config.src + '/ts/*.ts' )
+    .pipe( typescriptCompiler() )
+    .pipe( gulp.dest( config.distAssets + '/js' ) )
+}
 
-gulp.task( `css-lib:copy`, function () {
-  return gulp.src( path.src.cssLib )
-    .pipe( gulp.dest( path.dist.css ) );
-} );
+function compileStyle() {
+  return gulp.src( config.src + '/scss/main.scss' )
+    .pipe( sassCompiler() )
+    .pipe( gulp.dest( config.distAssets + '/css' ) )
+}
 
-gulp.task( `build`, gulp.series( `html:build`, `js:build`, `style:build`, `css-lib:copy`, `fonts:build`, `image:build` ) );
+function clear( cb ) {
+  fs.rmSync( config.dist, {
+    force: true,
+    recursive: true,
+  } )
 
-gulp.task( `watch`, function () {
-  gulp.watch( [ ...path.watch.html ], gulp.series( `html:build` ) );
-  gulp.watch( [ ...path.watch.style ], gulp.series( `style:build` ) );
-  gulp.watch( [ ...path.watch.js ], gulp.series( `js:build` ) );
-  gulp.watch( [ ...path.watch.img ], gulp.series( `image:build` ) );
-  gulp.watch( [ ...path.watch.fonts ], gulp.series( `fonts:build` ) );
-  gulp.watch( [ ...path.watch.cssLib ], gulp.series( `css-lib:copy` ) );
-} );
+  return cb()
+}
 
-gulp.task( `default`, gulp.series( `clean`, gulp.parallel( `build`, `server:lunch`, `watch` ) ) );
+function copyImages() {
+  return gulp.src( config.src + '/img/*.*' )
+    .pipe( gulp.dest( config.distAssets + '/img' ) )
+}
+
+function copyFonts() {
+  return gulp.src( config.src + '/fonts/*.*' )
+    .pipe( gulp.dest( config.distAssets + '/fonts' ) )
+}
+
+
+function copyStatic() {
+  return gulp.src( config.src + '/static/**/*.*' )
+    .pipe( gulp.dest( config.dist ) )
+}
+
+function watcher( cb ) {
+  gulp.watch( config.src + '/scss/**/*.scss', compileStyle )
+  gulp.watch( config.src + '/pages/**/*.pug', compilePug )
+  gulp.watch( config.src + '/ts/**/*.ts', compileTypescript )
+  gulp.watch( config.src + '/img/**/*.*', copyImages )
+  gulp.watch( config.src + '/fonts/**/*.*', copyFonts )
+  gulp.watch( config.src + '/static/**/*.*', copyStatic )
+
+  return cb()
+}
+
+function serve( cb ) {
+  BS.init( {
+    files: './src/**/*.*',
+    server: {
+      baseDir: './dist'
+    },
+    watch: true,
+    open: true,
+    tunnel: null,
+  } )
+
+  return cb()
+}
+
+
+
+const build = gulp.parallel( compilePug, compileStyle, compileTypescript, copyImages, copyFonts, copyStatic )
+
+
+
+module.exports.serve = gulp.series( watcher, serve )
+module.exports.build = build
+module.exports.dev = gulp.series( clear, build, watcher, serve )
