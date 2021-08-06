264adef21496f4c613d5e28ada6350de2ca211ce
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-08-06 23:13:02 +0300

    Update API LoginRequest: Stop on first validation failure

--- api/app/Http/Requests/LoginRequest.php
+++ api/app/Http/Requests/LoginRequest.php
@@ -4,11 +4,13 @@ namespace App\Http\Requests;
 
 class LoginRequest extends ApiRequest
 {
+    protected $stopOnFirstFailure = true;
+
     public function messages()
     {
         return [
             'login.required'            => 'Вы не указали логин',
-            'login.exists'               => 'Мы не смогли найти такого пользователя',
+            'login.exists'              => 'Мы не смогли найти такого пользователя',
             'password.required'         => 'Вы не указали пароль',
         ];
     }
