83a75e2bc2614d04017180d4f1abacbbea6e1924
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-08-07 19:25:29 +0300

    Add: logout functionality

--- web/src/ts/pages/home.ts
+++ web/src/ts/pages/home.ts
@@ -1,3 +1,24 @@
 import ColorSelect from '../modules/color-select.js'
+import ApiReq from '../modules/apiReq.js'
+import Token from '../modules/token.js'
+import _ from '../modules/queryLight.js'
 
 ColorSelect.init()
+
+_('.sidebar__exit').on('click', exit)
+
+const pageLoader = _('.loader')
+const pageBody = _('body')
+
+function exit() {
+  pageLoader.dataset.shown = true
+  pageBody.addClass('scroll-fix')
+  ApiReq
+    .send('logout', 'delete', null, true)
+    .then(response => {
+      if (response.data) {
+        Token.clear()
+        location.href = '/login.html'
+      } else delete pageLoader.dataset.shown
+    })
+}
