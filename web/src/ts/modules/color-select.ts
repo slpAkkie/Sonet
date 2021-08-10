2101336e6f69267a5fc148411de58d3432a4210a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-08-10 18:29:39 +0300

    Fix web: color-select
    There was a bug when open two selectors simultaneously

--- web/src/ts/modules/color-select.ts
+++ web/src/ts/modules/color-select.ts
@@ -21,7 +21,7 @@ export default class ColorSelect {
    * @returns {void}
    */
   static init(): void {
-    document.addEventListener('click', this.selectedValue_click.bind(this))
+    document.addEventListener('click', this.displayedValue_click.bind(this))
     document.addEventListener('click', this.listItem_click.bind(this))
   }
 
@@ -31,16 +31,15 @@ export default class ColorSelect {
    * @param {Event} evt
    * @returns {void}
    */
-  static selectedValue_click(evt): void {
+  static displayedValue_click(evt): void {
     // Check if click was dispatched on the selected value
     const selectedValue = (<HTMLElement>evt.target).closest('.color-select__selected-value')
-    if (!selectedValue) {
+    if (!selectedValue)
       if (!this.openedDropDown) return
-
-      // If we have opened drop-down menu close it
-      this.#closeAllDropDown()
-      return evt.preventDefault()
-    }
+      else {
+        this.#closeAllDropDown()
+        return evt.preventDefault()
+      }
 
     // Find color select element and the drop-down menu
     // and then toggle it's state
@@ -60,7 +59,7 @@ export default class ColorSelect {
     const listItem = <HTMLElement>(<HTMLElement>evt.target).closest('.color-select__item')
     if (!listItem) return
 
-    this.#setData(this.#getDataFromItem(listItem), this.#getSelectElements(listItem))
+    this.#setData(this.#getDataFromItem(listItem), this.#getSelectorElements(listItem))
   }
 
   static #setData(props, { selectedValue, selectedValueText, selectedValueColor }) {
@@ -93,7 +92,7 @@ export default class ColorSelect {
    * @param {HTMLElement} listItem
    * @returns {SelectElements}
    */
-  static #getSelectElements(listItem): SelectElements {
+  static #getSelectorElements(listItem: HTMLElement): SelectElements {
     const selectedValue = <HTMLElement>(<HTMLElement>listItem.closest('.color-select')).querySelector('.color-select__selected-value')
 
     return {
@@ -114,18 +113,42 @@ export default class ColorSelect {
   }
 
   /**
+   * Check in which direction open the drop-down menu
+   *
+   * @param {HTMLElement} dropDown
+   * @returns {void}
+   */
+  static #checkPosition(dropDown: HTMLElement): void {
+    document.documentElement.getBoundingClientRect().right < dropDown.getBoundingClientRect().right
+      ? dropDown.classList.add('color-select__drop-down_to-left')
+      : dropDown.classList.remove('color-select__drop-down_to-left')
+  }
+
+  /**
+   * Set drop-down menu opened
+   *
+   * @param {HTMLElement} dropDown
+   * @returns {void}
+   */
+  static #open(dropDown: HTMLElement): void {
+    this.openedDropDown = dropDown
+    this.openedDropDown.dataset.shown = 'true'
+  }
+
+  /**
    * Toggle drop-down menu state
    *
    * @param {HTMLElement} dropDown
    * @returns {void}
    */
-  static #toggleDropDown(dropDown): void {
+  static #toggleDropDown(dropDown: HTMLElement): void {
     if (this.openedDropDown) {
-      delete dropDown.dataset.shown
-      this.openedDropDown = null
-    } else {
-      dropDown.dataset.shown = 'true'
-      this.openedDropDown = dropDown
-    }
+      delete this.openedDropDown.dataset.shown
+
+      if (dropDown.isEqualNode(this.openedDropDown)) return this.openedDropDown = null
+      else this.#open(dropDown)
+    } else this.#open(dropDown)
+
+    this.#checkPosition(dropDown)
   }
 }
