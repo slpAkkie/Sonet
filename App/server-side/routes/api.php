50a5022a040a355136b392fe5d732f6b311dd40a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-10-09 11:20:04 +0300

    New: test route

--- App/server-side/routes/api.php
+++ App/server-side/routes/api.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
 Route::middleware(['api-token'])->delete('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+
+
+
+Route::get('/test', [\App\Http\Controllers\Controller::class, 'test'])->name('test');
