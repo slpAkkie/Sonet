fa8467902d1a44e9437203fa1349d214ad2fc127
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-10-10 17:12:01 +0300

    Prepare vue: step first

--- App/frontend/src/main.js
+++ App/frontend/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
+import vueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router).use(vueAxios, axios).mount('#app')
+app.axios.defaults.baseURL = 'http://api.localhost/'
--- App/frontend/src/store/index.js
+++ App/frontend/src/store/index.js
@@ -2,11 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    //
   },
   mutations: {
+    //
   },
   actions: {
+    //
   },
   modules: {
-  }
+    //
+  },
+  getters: {
+    //
+  },
 })
