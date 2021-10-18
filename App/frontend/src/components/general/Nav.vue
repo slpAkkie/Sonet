b8f6a0adbee81d0f001e4ab8a733f41a5cd0bc1f
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-10-18 22:27:17 +0300

    Update: nav
    # New:
    
    - Logo

--- App/frontend/src/components/general/Nav.vue
+++ App/frontend/src/components/general/Nav.vue
@@ -1,15 +1,26 @@
 <template>
-  <Button value="Выход" @click="tryLogout" />
+  <nav class="nav">
+    <div class="container">
+      <div class="nav__inner">
+        <div class="nav__left">
+          <img class="nav__logo" src="@/assets/img/nav__logo.png" alt="Logo">
+        </div>
+        <div class="nav__right">
+          <Button value="Выход" @click="tryLogout" />
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-import Button from "../elements/Button";
+import Button from "../elements/Button"
 
 export default {
   name: 'Nav',
   emits: [ 'logout' ],
   components: {
-    Button
+    Button,
   },
   methods: {
     tryLogout() {
@@ -25,3 +36,19 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.nav {
+  &__inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    //
+    padding: 1.5rem 0;
+  }
+
+  &__logo {
+    max-width: 10.5rem;
+  }
+}
+</style>
