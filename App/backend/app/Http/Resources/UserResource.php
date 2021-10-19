27590bff0d32f95aef1e7b300aa826c9569c4308
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-10-19 12:59:03 +0300

    Update: UserResource
    # Update:
    
    Remove id propertu from response

--- App/backend/app/Http/Resources/UserResource.php
+++ App/backend/app/Http/Resources/UserResource.php
@@ -7,7 +7,6 @@ class UserResource extends CommonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'login' => $this->login,
